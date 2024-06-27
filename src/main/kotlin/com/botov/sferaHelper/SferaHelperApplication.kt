67e3b47e74552718c556b6245c4879a78ed15f83
Botov Dmitriy <dbotov@vtb.ru>
2024-06-27 15:08:00 +0300

    add spring

---
+++ src/main/kotlin/com/botov/sferaHelper/SferaHelperApplication.kt
@@ -0,0 +1,11 @@
+package com.botov.sferaHelper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SferaHelperApplication
+
+fun main(args: Array<String>) {
+	runApplication<SferaHelperApplication>(*args)
+}
---
+++ src/test/kotlin/com/botov/sferaHelper/SferaHelperApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.botov.sferaHelper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SferaHelperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
