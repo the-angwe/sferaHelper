5d8fcb61ecdda65625131959c19c6eb093d97a3d
Botov Dmitriy <dbotov@vtb.ru>
2024-09-18 17:37:26 +0300

    refactoring

--- src/main/java/com/botov/sferaHelper/SferaHelperApplication.java
+++ src/main/java/com/botov/sferaHelper/SferaHelperApplication.java
@@ -5,21 +5,21 @@ import com.botov.sferaHelper.dto.TicketDto;
 import com.botov.sferaHelper.service.SferaService;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
 
 class SferaHelperApplication {
 		public static void main(String... args) throws IOException {
 			//String query = "area='RDS'";
-			String query = "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer') and systems = \"1672_3 Аутентификация подтверждение операций\" and parent = null";
-
+			//String query = "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer') and systems = \"1672 Аутентификация ФЛ РБ\" and parent = null";
+			String query = "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer') and estimation = null";
 			ListTicketsDto listTicketsDto = listTicketsByQuery(query);
 
 			for (TicketDto ticket: listTicketsDto.getContent()) {
-				setParent(ticket, "STROMS-2722");
-			}
-
-			/*for (TicketDto ticket: listTicketsDto.getContent()) {
+				//setSystem(ticket, "\"1672_3 Аутентификация подтверждение операций\"");
+				//setParent(ticket, "STROMS-2723");
 				setEstimation(ticket, 3600L);
-			}*/
+			}
 			System.out.println("end");
 		}
 
@@ -40,6 +40,13 @@ class SferaHelperApplication {
 	private static void setParent(TicketDto ticket, String parent) throws IOException {
 		TicketDto ticketDto = new TicketDto();
 		ticketDto.setParent(parent);
+		patchTicket(ticket.getNumber(), ticketDto);
+	}
+
+	//TODO not working
+	private static void setSystem(TicketDto ticket, String system) throws IOException {
+		TicketDto ticketDto = new TicketDto();
+		ticketDto.setSystems(Collections.singleton(system));
 		patchTicket(ticket.getNumber(), ticketDto);
 	}
 
--- src/main/java/com/botov/sferaHelper/dto/TicketDto.java
+++ src/main/java/com/botov/sferaHelper/dto/TicketDto.java
@@ -2,10 +2,13 @@ package com.botov.sferaHelper.dto;
 
 import lombok.Data;
 
+import java.util.Set;
+
 @Data
 public class TicketDto {
     private Long id;
     private String number;//like FRNRSA-5000
     private Long estimation;//in seconds
     private String parent;//like STROMS-1000
+    private Set<String> systems;//like STROMS-1000
 }
