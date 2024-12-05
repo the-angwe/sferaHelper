9feed57360947079fb71699f200b9d68bc29f1fa
Botov Dmitriy <dbotov@vtb.ru>
2024-12-05 18:21:04 +0300

    refactoring

--- src/main/java/com/botov/sferaHelper/SferaHelperTicketTypesFixer.java
+++ src/main/java/com/botov/sferaHelper/SferaHelperTicketTypesFixer.java
@@ -39,7 +39,7 @@ public class SferaHelperTicketTypesFixer {
             throw new RuntimeException();
         }*/
 
-        String query = "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer') and sprint = '4332'";
+        String query = "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer') and sprint = '4333'";
         ListTicketsDto listTicketsDto = SferaHelperMethods.listTicketsByQuery(query);
 
         HashMap<TicketType, List<GetTicketDto>> fullTicketsMap = new HashMap<>();
