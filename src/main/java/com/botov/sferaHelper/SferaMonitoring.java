4837b7b983b4e885e672040b7922ddef758691db
Botov Dmitriy <dbotov@vtb.ru>
2025-03-17 15:18:27 +0300

    SferaMonitoring

--- src/main/java/com/botov/sferaHelper/SferaMonitoring.java
+++ src/main/java/com/botov/sferaHelper/SferaMonitoring.java
@@ -6,18 +6,24 @@ import com.botov.sferaHelper.dto.ListTicketsDto;
 import com.botov.sferaHelper.service.SferaHelperMethods;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class SferaMonitoring {
 
     public static void main(String... args) throws IOException {
-        checkProdBugs();
+        //checkProdBugs();
         checkTicketsWithoutEpics();
         checkTicketsWithoutEstimation();
         checkTicketsWithoutSprint();
         checkTicketsWithWrongSystems();
         checkTicketsWithWrongProject();
+        checkCreatedRDSs();
+        checkOverdueRDSs();
+
+        //checkRDSs();
 
 
         //истории без критериев приёмки
@@ -26,17 +32,64 @@ public class SferaMonitoring {
 
         //новые этики на мне
         //эпики без оценок, без критериев приёмка, без декопозиции
-        //просроченные РДСы и РДСы в статус "создано" или с открытыми вопросами
+
+    }
+
+    private static void checkOverdueRDSs() throws IOException {
+        //просроченные РДСы
+        String dueDate = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+        String query = "area='RDS' and status not in ('closed', 'done', 'rejectedByThePerformer') and " +
+                "((dueDate = null) or (dueDate < '"
+                + dueDate +
+                "')) and assignee in (\"vtb70166052@corp.dev.vtb\", \"vtb4065673@corp.dev.vtb\", \"vtb70190852@corp.dev.vtb\", \"vtb4075541@corp.dev.vtb\", \"vtb4078565@corp.dev.vtb\", \"VTB4075541@corp.dev.vtb\")";
+        ListTicketsDto listTicketsDto = SferaHelperMethods.listTicketsByQuery(query);
+
+        System.err.println();
+        System.err.println("просроченные РДСы (кол-во " + listTicketsDto.getContent().size() + "):");
+        for (ListTicketShortDto ticket: listTicketsDto.getContent()) {
+            System.err.println(ticket.getNumber());
+        }
+    }
+
+    private static void checkRDSs() throws IOException {
+        //просроченные РДСы или с открытыми вопросами
+        String query = "area='RDS' and status not in ('closed', 'done', 'rejectedByThePerformer') and assignee in (\"vtb70166052@corp.dev.vtb\", \"vtb4065673@corp.dev.vtb\", \"vtb70190852@corp.dev.vtb\", \"vtb4075541@corp.dev.vtb\", \"vtb4078565@corp.dev.vtb\", \"VTB4075541@corp.dev.vtb\")";
+        ListTicketsDto listTicketsDto = SferaHelperMethods.listTicketsByQuery(query);
+
+        List<GetTicketDto> prodDefects = new ArrayList<>();
+        for (ListTicketShortDto listTicketShortDto: listTicketsDto.getContent()) {
+            GetTicketDto ticket = SferaHelperMethods.ticketByNumber(listTicketShortDto.getNumber());
+        }
+
+        System.out.println();
+        System.err.println("дефекты прода (кол-во " + listTicketsDto.getContent().size() + "):");
+        for (GetTicketDto ticket : prodDefects) {
+            System.out.println(ticket.getNumber());
+        }
+    }
+
+    private static void checkCreatedRDSs() throws IOException {
+        //RDS в статусe "Создано"
+        String query = "area='RDS' and status in ('created') and assignee in (\"vtb70166052@corp.dev.vtb\", \"vtb4065673@corp.dev.vtb\", \"vtb70190852@corp.dev.vtb\", \"vtb4075541@corp.dev.vtb\", \"vtb4078565@corp.dev.vtb\", \"VTB4075541@corp.dev.vtb\")";
+        ListTicketsDto listTicketsDto = SferaHelperMethods.listTicketsByQuery(query);
+
+        System.err.println();
+        System.err.println("RDS в статусe \"Создано\" (кол-во " + listTicketsDto.getContent().size() + "):");
+        for (ListTicketShortDto ticket: listTicketsDto.getContent()) {
+            System.err.println(ticket.getNumber());
+        }
     }
 
     private static void checkTicketsWithWrongProject() throws IOException {
         //задачи с неправильным проектом (не 2973)
         String query = "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer') and projectConsumer != 'f9696ccf-0f8d-431e-a803-9d00ee6e3329'";
         ListTicketsDto listTicketsDto = SferaHelperMethods.listTicketsByQuery(query);
+
+        System.err.println();
         System.err.println("задачи с неправильным проектом (не 2973) (кол-во " + listTicketsDto.getContent().size() + "):");
         for (ListTicketShortDto ticket: listTicketsDto.getContent()) {
             SferaHelperMethods.setProject(ticket.getNumber(), "f9696ccf-0f8d-431e-a803-9d00ee6e3329");// проект 2973
-            System.out.println(ticket.getNumber());
+            System.err.println(ticket.getNumber());
         }
     }
 
@@ -44,10 +97,12 @@ public class SferaMonitoring {
         //Задачи не по 1553 (особенно по 1672_3)
         String query = "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer') and systems != \"1553 Заявки ФЛ\"";
         ListTicketsDto listTicketsDto = SferaHelperMethods.listTicketsByQuery(query);
+
+        System.err.println();
         System.err.println("Задачи не по 1553 (кол-во " + listTicketsDto.getContent().size() + "):");
         for (ListTicketShortDto ticket: listTicketsDto.getContent()) {
             SferaHelperMethods.setSystem(ticket.getNumber(), "\"1553 Заявки ФЛ\"");
-            System.out.println(ticket.getNumber());
+            System.err.println(ticket.getNumber());
         }
     }
 
@@ -65,10 +120,10 @@ public class SferaMonitoring {
             }
         }
 
-        System.out.println();
+        System.err.println();
         System.err.println("дефекты прода (кол-во " + listTicketsDto.getContent().size() + "):");
         for (GetTicketDto ticket : prodDefects) {
-            System.out.println(ticket.getNumber());
+            System.err.println(ticket.getNumber());
         }
     }
 
@@ -76,9 +131,11 @@ public class SferaMonitoring {
         //задачи без эпиков
         String query = "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer') and parent = null";
         ListTicketsDto listTicketsDto = SferaHelperMethods.listTicketsByQuery(query);
+
+        System.err.println();
         System.err.println("задачи без эпиков (кол-во " + listTicketsDto.getContent().size() + "):");
         for (ListTicketShortDto ticket: listTicketsDto.getContent()) {
-            System.out.println(ticket.getNumber());
+            System.err.println(ticket.getNumber());
         }
     }
 
@@ -86,10 +143,12 @@ public class SferaMonitoring {
         //задачи без оценок
         String query = "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer') and estimation = null";
         ListTicketsDto listTicketsDto = SferaHelperMethods.listTicketsByQuery(query);
+
+        System.err.println();
         System.err.println("задачи без оценок (кол-во " + listTicketsDto.getContent().size() + "):");
         for (ListTicketShortDto ticket: listTicketsDto.getContent()) {
             SferaHelperMethods.setEstimation(ticket.getNumber(), 3600L);
-            System.out.println(ticket.getNumber());
+            System.err.println(ticket.getNumber());
         }
     }
 
@@ -97,9 +156,11 @@ public class SferaMonitoring {
         //задачи вне спринтов
         String query = "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer') and sprint = null";
         ListTicketsDto listTicketsDto = SferaHelperMethods.listTicketsByQuery(query);
+
+        System.err.println();
         System.err.println("задачи вне спринтов (кол-во " + listTicketsDto.getContent().size() + "):");
         for (ListTicketShortDto ticket: listTicketsDto.getContent()) {
-            System.out.println(ticket.getNumber());
+            System.err.println(ticket.getNumber());
         }
     }
 }
