594c7352a4af08c642f12b23aeaa538f8c33edf7
Botov Dmitriy <dbotov@vtb.ru>
2025-04-23 23:36:31 +0300

    SferaMonitoring

--- src/main/java/com/botov/sferaHelper/SferaMonitoring.java
+++ src/main/java/com/botov/sferaHelper/SferaMonitoring.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class SferaMonitoring {
 
     public static void main(String... args) throws IOException {
-        //checkProdBugs();
+        checkProdBugs();
         checkTicketsWithoutEpics();
         checkTicketsWithoutEstimation();
         checkTicketsWithoutSprint();
