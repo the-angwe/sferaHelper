b52d24792a5f0cdfc986217b62b3b1ad329c356c
Botov Dmitriy <dbotov@vtb.ru>
2025-09-16 14:39:54 +0300

    SferaHelperTicketCopier

---
+++ src/main/java/com/botov/sferaHelper/dto/PatchSprintDto.java
@@ -0,0 +1,8 @@
+package com.botov.sferaHelper.dto;
+
+import lombok.Data;
+
+@Data
+public class PatchSprintDto {
+    private int id; //like 4352
+}
--- src/main/java/com/botov/sferaHelper/service/SferaHelperMethods.java
+++ src/main/java/com/botov/sferaHelper/service/SferaHelperMethods.java
@@ -85,7 +85,12 @@ public class SferaHelperMethods {
             attribute.setAttribute("sprint");
             SferaService.INSTANCE.deleteAttributes(List.of(attribute)).execute();
         } else {
-            throw new RuntimeException("Not implemented");
+            //throw new RuntimeException("Not implemented");
+            PatchTicketDto ticketDto = new PatchTicketDto();
+            PatchSprintDto patchSprintDto = new PatchSprintDto();
+            patchSprintDto.setId(Integer.parseInt(sprint));
+            ticketDto.setSprint(Collections.singleton(patchSprintDto));
+            patchTicket2(number, ticketDto);
         }
     }
 
