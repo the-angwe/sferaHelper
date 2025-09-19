c76f1cb8077ef023191fe0785b56790b7eba2959
Botov Dmitriy <dbotov@vtb.ru>
2025-09-19 10:48:53 +0300

    SferaHelperTicketCopier

--- src/main/java/com/botov/sferaHelper/SferaHelperApplication.java
+++ src/main/java/com/botov/sferaHelper/SferaHelperApplication.java
@@ -13,9 +13,10 @@ class SferaHelperApplication {
 			//String query = "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer') and parent=null";
 			//String query = "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer') and systems = \"1553 Заявки ФЛ\"";
 			//String query = "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer') and systems = \"1672_3 Аутентификация подтверждение операций\"";
-			String query = "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer')";
+			String query = "area=\"FRNRSA\" and systems = \"1672_3 Аутентификация подтверждение операций\"";
+			//String query = "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer')";
 			//String query = "area=\"FRNRSA\" and updateDate > '2024-09-30'";
-			//String query = "area=\"FRNRSA\" and number='FRNRSA-8083'";
+			//String query = "area=\"FRNRSA\" and number='FRNRSA-7669'";
 			//String query =  "area=\"FRNRSA\" and status not in ('closed', 'done', 'rejectedByThePerformer') and systems = \"1672_3 Аутентификация подтверждение операций\"";
 
 			//String query =  "area='RDS' and status not in ('closed', 'done', 'rejectedByThePerformer') and assignee in (\"vtb70166052@corp.dev.vtb\", \"vtb4065673@corp.dev.vtb\", \"vtb70190852@corp.dev.vtb\", \"vtb4075541@corp.dev.vtb\", \"vtb4078565@corp.dev.vtb\", \"VTB4075541@corp.dev.vtb\") and name ~ '1553'";
@@ -25,12 +26,12 @@ class SferaHelperApplication {
 			ListTicketsDto listTicketsDto = SferaHelperMethods.listTicketsByQuery(query);
 
 			for (ListTicketShortDto ticket: listTicketsDto.getContent()) {
-				//SferaHelperMethods.setSystem(ticket, "\"1672_3 Аутентификация подтверждение операций\"");
+				//SferaHelperMethods.setSystem(ticket.getNumber(), "1672_3 Аутентификация подтверждение операций");
 				//SferaHelperMethods.setParent(ticket.getNumber(), "STROMS-4792");
 				//SferaHelperMethods.setEstimation(ticket.getNumber(), 3600L);
-				SferaHelperMethods.setDueDate(ticket.getNumber(), "2025-12-31");
+				//SferaHelperMethods.setDueDate(ticket.getNumber(), "2025-12-31");
 				//SferaHelperMethods.setProject(ticket.getNumber(), "f9696ccf-0f8d-431e-a803-9d00ee6e3329");// проект 2973
-				//SferaHelperMethods.setSystem(ticket.getNumber(), "\"1553 Заявки ФЛ\"");
+				SferaHelperMethods.setSystem(ticket.getNumber(), "1553 Заявки ФЛ");
 				//if (ticket.getSystems() != null && !ticket.getSystems().isEmpty() && !ticket.getSystems().contains("1553 Заявки ФЛ")) {
 				//SferaHelperMethods.setSystem(ticket.getNumber(), "1553 Заявки ФЛ");
 				//}
